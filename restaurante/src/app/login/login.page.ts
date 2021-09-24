d27af57b7f49f26a4f4a62ee8123118dadcc7489
512kbb <dieg.parraa@duocuc.cl>
2021-09-24 10:34:07 -0300

    ??

--- restaurante/src/app/login/login.page.ts
+++ restaurante/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
+  login(form) {
+    var usuario = form.value["usuario"];
+    var contrasena = form.value["password"];
+
+    if (usuario == "admin" && contrasena == "123") {
+      console.log('admin entrance noises')
+      this.router.navigate(['productos'])
+    }
+  }
 }
